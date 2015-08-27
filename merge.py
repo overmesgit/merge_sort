dfbe68abf941fad6a5a87584f8062dcc5ceec265
overmes <a.bezukladichny@securitycode.ru>
2015-08-27 12:24:50 +0300

    merge sort

--- merge.py
+++ merge.py
@@ -1,2 +1,28 @@
+from operator import lt, gt
+
+
 def merge_sort(input_array, reverse=False):
+    input_len = len(input_array)
+    operator = lt if reverse else gt
+
+    if input_len > 2:
+        slice_index = int(input_len / 2)
+        left_part = merge_sort(input_array[:slice_index], reverse)
+        right_part = merge_sort(input_array[slice_index:], reverse)
+
+        l, r = 0, 0
+        for k in range(input_len):
+            if r == len(right_part) or l < len(left_part) and not operator(left_part[l], right_part[r]):
+                input_array[k] = left_part[l]
+                l += 1
+            else:
+                input_array[k] = right_part[r]
+                r += 1
+    else:
+        if input_len <= 1:
+            return input_array
+        else:
+            if operator(input_array[0], input_array[1]):
+                input_array.reverse()
+
     return input_array
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from timeit import timeit
 from unittest.case import TestCase
 from bubble import bubble_sort
 from merge import merge_sort
@@ -22,3 +23,10 @@ class SortTest(TestCase):
         for test_case in self.case_gen():
             self.assertEqual(sorted(test_case), bubble_sort(test_case))
             self.assertEqual(sorted(test_case, reverse=True), bubble_sort(test_case, reverse=True))
+
+    def test_time(self):
+        n = 1000
+        test_input = sample(range(n), n)
+        print('Native sort: {}'.format(timeit(lambda: test_input.copy().sort(), number=10)))
+        print('Bubble sort: {}'.format(timeit(lambda: bubble_sort(test_input.copy()), number=10)))
+        print('Merge sort: {}'.format(timeit(lambda: merge_sort(test_input.copy()), number=10)))
