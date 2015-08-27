493eff1a4934de2f3e250008c92aa4663d87bef0
overmes <a.bezukladichny@securitycode.ru>
2015-08-27 15:49:27 +0300

    not recursion merge sort

--- merge.py
+++ merge.py
@@ -12,7 +12,7 @@ def merge_sort(input_array, reverse=False):
 
         l, r = 0, 0
         for k in range(input_len):
-            if r == len(right_part) or l < len(left_part) and not operator(left_part[l], right_part[r]):
+            if r == len(right_part) or l != len(left_part) and not operator(left_part[l], right_part[r]):
                 input_array[k] = left_part[l]
                 l += 1
             else:
@@ -24,5 +24,38 @@ def merge_sort(input_array, reverse=False):
         else:
             if operator(input_array[0], input_array[1]):
                 input_array.reverse()
+
+    return input_array
+
+
+def merge_sort_not_recursion(input_array, reverse=False):
+    input_copy = input_array.copy()
+    input_length = len(input_copy)
+    operator = lt if reverse else gt
+
+    # optimization for merge_length 1
+    # for start in range(0, input_length, 2):
+    #     end = start + 1
+    #     if end < input_length and operator(input_copy[start], input_copy[end]):
+    #         input_copy[start], input_copy[end] = input_copy[end], input_copy[start]
+
+    merge_length = 1
+    while merge_length < input_length:
+        for l in range(0, input_length, merge_length*2):
+            r = min(l + merge_length, input_length)
+            l_last = r
+            r_last = min(r + merge_length, input_length)
+
+            for k in range(l, r_last):
+                if r == r_last or l != l_last and not operator(input_copy[l], input_copy[r]):
+                    input_array[k] = input_copy[l]
+                    l += 1
+                else:
+                    input_array[k] = input_copy[r]
+                    r += 1
+
+        merge_length *= 2
+        if merge_length < input_length:
+            input_array, input_copy = input_copy, input_array
 
     return input_array
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from timeit import timeit
 from unittest.case import TestCase
 from bubble import bubble_sort
-from merge import merge_sort
+from merge import merge_sort, merge_sort_not_recursion
 from random import sample
 
 
@@ -10,7 +10,7 @@ class SortTest(TestCase):
         def test_case_gen(start=5, factor=125, number=5):
             yield []
             yield [5]
-            yield from (sample(range(i), i) for i in range(start, factor*number + start, factor))
+            yield from (sample(range(i), i) for i in range(start, factor * number + start, factor))
 
         self.case_gen = test_case_gen
 
@@ -24,9 +24,15 @@ class SortTest(TestCase):
             self.assertEqual(sorted(test_case), bubble_sort(test_case))
             self.assertEqual(sorted(test_case, reverse=True), bubble_sort(test_case, reverse=True))
 
+    def test_merge_sort_not_recursion(self):
+        for test_case in self.case_gen():
+            self.assertEqual(sorted(test_case), merge_sort_not_recursion(test_case))
+            self.assertEqual(sorted(test_case, reverse=True), merge_sort_not_recursion(test_case, reverse=True))
+
     def test_time(self):
-        n = 1000
+        n = 500
         test_input = sample(range(n), n)
-        print('Native sort: {}'.format(timeit(lambda: test_input.copy().sort(), number=10)))
-        print('Bubble sort: {}'.format(timeit(lambda: bubble_sort(test_input.copy()), number=10)))
-        print('Merge sort: {}'.format(timeit(lambda: merge_sort(test_input.copy()), number=10)))
+        test_functions = (bubble_sort, merge_sort, merge_sort_not_recursion)
+        for func in test_functions:
+            runing_time = timeit(lambda: func(test_input.copy()), number=10)
+            print('{}: {}'.format(func.__name__, runing_time))
