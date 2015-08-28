94dac5cd07c400c006fb3051af7cbd600dfc6e5a
overmes <a.bezukladichny@securitycode.ru>
2015-08-28 09:52:32 +0300

    refactoring merge sort

--- merge.py
+++ merge.py
@@ -3,27 +3,27 @@ from operator import lt, gt
 
 def merge_sort(input_array, reverse=False):
     input_len = len(input_array)
-    operator = lt if reverse else gt
+    operator = gt if reverse else lt
 
-    if input_len > 2:
+    # optimization for merge_length 1
+    # if input_len == 2:
+    #     if operator(input_array[1], input_array[0]):
+    #         input_array.reverse()
+    #     return input_array
+
+    if input_len > 1:
         slice_index = int(input_len / 2)
         left_part = merge_sort(input_array[:slice_index], reverse)
         right_part = merge_sort(input_array[slice_index:], reverse)
 
         l, r = 0, 0
         for k in range(input_len):
-            if r == len(right_part) or l != len(left_part) and not operator(left_part[l], right_part[r]):
+            if r == len(right_part) or l != len(left_part) and operator(left_part[l], right_part[r]):
                 input_array[k] = left_part[l]
                 l += 1
             else:
                 input_array[k] = right_part[r]
                 r += 1
-    else:
-        if input_len <= 1:
-            return input_array
-        else:
-            if operator(input_array[0], input_array[1]):
-                input_array.reverse()
 
     return input_array
 
@@ -31,7 +31,7 @@ def merge_sort(input_array, reverse=False):
 def merge_sort_not_recursion(input_array, reverse=False):
     input_copy = input_array.copy()
     input_length = len(input_copy)
-    operator = lt if reverse else gt
+    operator = gt if reverse else lt
     merge_length = 1
 
     # optimization for merge_length 1
@@ -48,7 +48,7 @@ def merge_sort_not_recursion(input_array, reverse=False):
             r_last = min(r + merge_length, input_length)
 
             for k in range(l, r_last):
-                if r == r_last or l != l_last and not operator(input_array[l], input_array[r]):
+                if r == r_last or l != l_last and operator(input_array[l], input_array[r]):
                     input_copy[k] = input_array[l]
                     l += 1
                 else:
