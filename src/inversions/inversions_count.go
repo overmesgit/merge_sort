0267fed43d8b0147e6d2ffdf17304a6d72da2a78
overmes <a.bezukladichny@securitycode.ru>
2015-09-03 12:31:00 +0300

    inversions

--- src/inversions/inversions_count.go
+++ src/inversions/inversions_count.go
@@ -8,17 +8,47 @@ import (
 )
 
 func main() {
-    data, _ := ioutil.ReadFile("src/inversions/IntegerArray.txt")
+    data, err := ioutil.ReadFile("src/inversions/IntegerArray.txt")
+    fmt.Println(err)
     string_data := string(data)
-    split_data := strings.Split(string_data, "\n")
+    split_data := strings.Split(string_data, "\r\n")
+    split_data = split_data[:len(split_data) - 1]
     input := make([]int, len(split_data))
+
+    fmt.Println(split_data[0], split_data[len(input) - 1])
+    fmt.Println(len(input))
     for i, str_num := range split_data {
         input[i], _ = strconv.Atoi(str_num)
     }
-
-    fmt.Println("Result: ")
+    _, res := InversionsCounter(input)
+    fmt.Println("Result: ", res)
 }
 
-func InversionsCounter(input []int) int {
-    return 0
+func InversionsCounter(input []int) ([]int, int) {
+    inputLength := len(input)
+    leftSplits, rightSplits, commonSplits := 0, 0, 0
+
+    if inputLength > 1 {
+        splitLength := inputLength/2
+        left, right := make([]int, splitLength), make([]int, inputLength - splitLength)
+        copy(left, input[:splitLength])
+        copy(right, input[splitLength:])
+
+        left, leftSplits = InversionsCounter(left)
+        right, rightSplits = InversionsCounter(right)
+        l, r := 0, 0
+        for k := range input {
+            if r == len(right) || l != len(left) && left[l] <= right[r] {
+                input[k] = left[l]
+                l++
+            } else {
+                input[k] = right[r]
+                r++
+                commonSplits += len(left) - l
+            }
+        }
+
+    }
+
+    return input, leftSplits + rightSplits + commonSplits
 }
--- src/inversions/inversions_test.go
+++ src/inversions/inversions_test.go
@@ -6,14 +6,19 @@ import (
 
 func TestSort(t *testing.T) {
     input := []map[string][]int{}
+    input = append(input, map[string][]int{"test": {1, 2}, "res": {0}})
+    input = append(input, map[string][]int{"test": {2, 1}, "res": {1}})
     input = append(input, map[string][]int{"test": {1, 2, 3}, "res": {0}})
     input = append(input, map[string][]int{"test": {3, 2, 1}, "res": {3}})
+    input = append(input, map[string][]int{"test": {1, 3, 5, 6, 2, 7}, "res": {3}})
+    input = append(input, map[string][]int{"test": {1, 3, 5, 2, 4, 6}, "res": {3}})
+    input = append(input, map[string][]int{"test": {1, 1, 1, 1, 1, 1}, "res": {0}})
 
     for _, testCase := range input {
-        result := InversionsCounter(testCase["test"])
+        array, result := InversionsCounter(testCase["test"])
 
         if result != testCase["res"][0] {
-            t.Errorf("input: %v inversions: %v != %v", testCase["test"], testCase["res"][0], result)
+            t.Errorf("input: %v inversions: %v != %v", array, testCase["res"][0], result)
         }
     }
     fmt.Println("Done")
